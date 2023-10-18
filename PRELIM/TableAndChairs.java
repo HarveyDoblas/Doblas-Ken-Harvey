16f0ef9d1445a1195a359061dc3f08892134a1a5
HarveyDoblas <148032936+HarveyDoblas@users.noreply.github.com>
2023-10-18 06:31:28 +0100

    Add files via upload

---
+++ PRELIM/TableAndChairs.java
@@ -0,0 +1,19 @@
+/* Ken Harvey Doblas 2B
+   9/13/23
+   Activity#1
+*/
+
+
+class TableAndChairs{
+ 
+ public static void main(String[] args){
+ 
+ System.out.println("X                    X");
+ System.out.println("X                    X");
+ System.out.println("X     XXXXXXXXXX     X");
+ System.out.println("XXXXX X        X XXXXX");
+ System.out.println("X   X X        X X   X");
+ System.out.println("X   X X        X X   X");
+              
+}
+}
